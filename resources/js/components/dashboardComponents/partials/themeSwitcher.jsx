ae103ebca7453972d8d41bc3125806062256ea6e
DOUHADJI <58432265+DOUHADJI@users.noreply.github.com>
2023-07-05 01:54:30 +0000

    dark mode enabled

--- resources/js/components/dashboardComponents/partials/themeSwitcher.jsx
+++ resources/js/components/dashboardComponents/partials/themeSwitcher.jsx
@@ -1,15 +1,18 @@
 
 import { Switch } from '@nextui-org/react'
 import { useTheme } from 'next-themes'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import { BsSun, BsMoon } from 'react-icons/bs'
 
 const ThemeSwitcher = () => {
   
-  const { theme, setTheme } = useTheme()
+ // const { theme, setTheme } = useTheme()
+ 
+  const [theme, setTheme ] = useState('light')
 
   useEffect(() => {
-    document.body.className = theme;
+    document.body.className = theme + " hold-transition font-sans font-nunito overflow-x-hidden bg-[#f4f6f9]";
   }, [theme]);
 
   return (
@@ -17,6 +20,7 @@ const ThemeSwitcher = () => {
       <Switch
         color={'warning'}
         size={"lg"}
+        checked={theme == 'dark' ? true : false}
         onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
         iconOn={<BsMoon className="dark:text-black" />}
         iconOff={<BsSun className="text-black" />}
