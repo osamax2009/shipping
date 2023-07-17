18f6986ddecbf34ea2dea7a7cedbbf8c798fe985
DOUHADJI <58432265+DOUHADJI@users.noreply.github.com>
2023-07-17 22:40:48 +0000

    prod version

--- app/Http/Controllers/API/PaypalController.php
+++ app/Http/Controllers/API/PaypalController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Models\PaymentGateway;
 use Illuminate\Http\Request;
 use PayPal\Rest\ApiContext;
